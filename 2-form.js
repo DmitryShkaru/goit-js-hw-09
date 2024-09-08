6740a9b8dad1d9a7b3839bd192070863c2f8ac02
DmitryShkaru <127674561+DmitryShkaru@users.noreply.github.com>
2024-09-08 16:27:31 +0000

    Deploying to gh-pages from @ DmitryShkaru/goit-js-hw-09@4296fa20681e8b2e22e02bdbba21b671ed9f9cd1 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/styles-B4s-4mBC.js";console.log("form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("form");
 //# sourceMappingURL=2-form.js.map
